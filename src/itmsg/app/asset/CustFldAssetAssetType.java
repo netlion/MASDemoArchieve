34ddc3dac19e3c68ee30155b74772b8a4a0d3b11
netlion <netlion72@gmail.com>
2025-03-11 14:11:04 +0900

    Update .gitignore and remove cached out directory

---
+++ src/itmsg/app/asset/CustFldAssetAssetType.java
@@ -0,0 +1,19 @@
+package itmsg.app.asset;
+
+import psdi.app.asset.FldAssetAssetType;
+import psdi.mbo.MboValue;
+import psdi.util.MXException;
+
+import java.rmi.RemoteException;
+
+public class CustFldAssetAssetType extends FldAssetAssetType {
+    public CustFldAssetAssetType(MboValue mbv) {
+        super(mbv);
+    }
+
+    @Override
+    public void initValue() throws MXException, RemoteException {
+        super.initValue();
+        System.out.println("Called CustFldAssetAssetType!!!!!!6!!");
+    }
+}
